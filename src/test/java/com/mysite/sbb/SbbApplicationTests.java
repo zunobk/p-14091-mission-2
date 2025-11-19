4493cae7d29ebbab9eaf57eaa88ff0f523499c5d
zunobk <junho4560@naver.com>
2025-11-19 15:34:11 +0900

    h2 추가, dev와 test 프로파일 분리

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,12 +2,14 @@ package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

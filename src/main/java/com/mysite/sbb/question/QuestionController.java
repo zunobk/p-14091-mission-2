e8e3ea695384f9b7b1096c5c1a97d7970542decd
zunobk <junho4560@naver.com>
2025-11-21 11:28:54 +0900

    타임리프 없이, /question/list 접속 시 질문 목록 구현

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,48 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    @ResponseBody
+    public String list() {
+        List<Question> questions = questionRepository.findAll();
+
+        String questionLi = questions
+                .stream()
+                .map(q -> "<li>%d / %s</li>".formatted(q.getId(), q.getSubject()))
+                .collect(Collectors.joining("\n"));
+
+        return """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>질문 목록</title>
+                <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+                <style>
+            
+                html {
+                  font-family: "Pretendard Variable";
+                }
+                </style>
+            </head>
+            <body>
+                <ul class="p-10">
+                    %s
+                </ul>
+            </body>
+            </html>
+            """.formatted(questionLi);
+    }
+}
